6ce06a1b827844450c9fef27fb946f916a39bb62
Kate Bo <bodrakea@gmail.com>
2024-01-09 22:12:09 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 13676;
+        int miles = ticketPrice / 20;
+        System.out.println("You've been credited " + miles + " miles");
+
+    }
+}
